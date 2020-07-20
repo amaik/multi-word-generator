6b7a9b7cc720fa72106a98646614ba3520c5087d
Alex Speicher <alex.speicher@gmx.net>
2020-07-20 13:37:49 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+
+
+GS_FOLDER_BERT = "gs://cloud-tpu-checkpoints/bert/keras_bert/uncased_L-12_H-768_A-12"
+LOCAL_FOLDER_BERT = "C:/Users/alexs/models/bert/uncased_L-12_H-768_A-12_2/"
+LOCAL_FOLDER_BERT_ASSETS = os.path.join(LOCAL_FOLDER_BERT, "assets")
