c6f82e171ea2ed2320c3f4aa13d7e2290258dfa0
Alex Speicher <alex.speicher@gmx.net>
2020-07-22 14:20:46 +0200

    Update model.py
    Finish first draft of model implementation

--- model.py
+++ model.py
@@ -12,21 +12,29 @@ import official.nlp.bert.bert_models
 # local imports
 import constants
 
-bert_config_file = os.path.join(constants.LOCAL_FOLDER_BERT, "bert_config.json")
-with open(bert_config_file, "r+") as f:
-    config_dict = json.loads(f.read())
 
-bert_config = bert.configs.BertConfig.from_dict(config_dict)
-
-bert_classifier, bert_encoder = bert.bert_models.classifier_model(
-    bert_config, num_labels=2)
+# show download progressbar
+TFHUB_PROGRESS_VAR = 'TFHUB_DOWNLOAD_PROGRESS'
+os.environ[TFHUB_PROGRESS_VAR] = "1"
 
 
-tf.keras.utils.plot_model(bert_encoder, show_shapes=True, dpi=48)
+def main():
+    bert_config_file = os.path.join(constants.LOCAL_FOLDER_BERT, "bert_config.json")
+    with open(bert_config_file, "r+") as f:
+        config_dict = json.loads(f.read())
+
+    bert_config = bert.configs.BertConfig.from_dict(config_dict)
+
+    # bert_classifier, bert_encoder = bert.bert_models.classifier_model(
+    #    bert_config, num_labels=2)
+    print("Bert config loaded.")
+    model = multiword_model(bert_config)
+    print("Model loaded")
+    tf.keras.utils.plot_model(model, show_shapes=True, dpi=48)
+
 
 def multiword_model(bert_config,
-                    max_seq_length,
-                    vocab_size,
+                    max_seq_length=128,
                     hub_url_bert_encoder=constants.HUB_URL_BERT,
                     hub_module_trainable=True,
                     final_layer_initializer=None):
@@ -47,23 +55,28 @@ def multiword_model(bert_config,
     pooled_out, seq_out = bert_model([input_word_ids, input_mask, input_type_ids])
 
     pooled_out = tf.keras.layers.Dropout(rate=bert_config.hidden_dropout_prob)(pooled_out)
-    #pooled_out = tf.keras.layers.Dense(bert_config.hidden, activation="gelu")(pooled_out)
-    pooled_out = tf.keras.layers.Dense(1, name="output_num_words")(pooled_out) # indicate the number of words to generate
+    # pooled_out = tf.keras.layers.Dense(bert_config.hidden, activation="gelu")(pooled_out)
+    pooled_out = tf.keras.layers.Dense(1, kernel_initializer=initializer, name="output_num_words")(
+        pooled_out)  # indicate the number of words to generate
 
-    seq_out = tf.keras.layers.Flatten()(seq_out)
-    seq_out = tf.keras.layers.Dense(seq_out.shape[1])(seq_out)
-    seq_out = tf.keras.layers.Dropout(bert_config.hidden_dropout_prob)(seq_out)
-    seq_out = tf.keras.layers.Dense(bert_config.vocab_size, )
-
-    # implementation reference: https://github.com/tensorflow/models/blob/master/official/nlp/bert/bert_models.py
+    seq_out = tf.keras.layers.Flatten()(seq_out) # TODO find out how I can dynically slice this to the row that relates to the [MASK] token
+    seq_out = tf.keras.layers.Dropout(bert_config.hidden_dropout_prob)(
+        seq_out)  # TODO should this be the same dropout prob?
+    seq_out = tf.keras.layers.Dense(bert_config.hidden_size,kernel_initializer=initializer)(seq_out) # TODO smaller intermediate layer for less weights, how much can and should I increase this?
+    seq_out = tf.keras.layers.Dense(bert_config.vocab_size, kernel_initializer=initializer, name="output_order_words")(
+        seq_out)
 
     return tf.keras.Model(
-      inputs={
-          'input_word_ids': input_word_ids,
-          'input_mask': input_mask,
-          'input_type_ids': input_type_ids
-      },
-      outputs={
-          'output_num_words':pooled_out,
+        inputs={
+            'input_word_ids': input_word_ids,
+            'input_mask': input_mask,
+            'input_type_ids': input_type_ids
+        },
+        outputs={
+            'output_num_words': pooled_out,
+            'output_order_words': seq_out
+        })
 
-      }), bert_model
+
+if __name__ == "__main__":
+    main()
