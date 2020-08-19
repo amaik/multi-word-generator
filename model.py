47ac8b0cd696e01097b30984280f2563e0e986a8
Alex Speicher <alex.speicher@gmx.net>
2020-08-19 10:05:50 +0200

    Update model.py
    Fix plot_model function when running as script

--- model.py
+++ model.py
@@ -1,43 +1,33 @@
-import os
 import json
+import os
 
+import fire
+import official.nlp.bert.configs
 import tensorflow as tf
 import tensorflow_hub as hub
-
 from official.nlp import bert
 
-import official.nlp.bert.configs
-import official.nlp.bert.bert_models
-
-# local imports
 import constants
-
 
 # show download progressbar
 TFHUB_PROGRESS_VAR = 'TFHUB_DOWNLOAD_PROGRESS'
 os.environ[TFHUB_PROGRESS_VAR] = "1"
 
 
-def main():
+def plot_model():
     bert_config_file = os.path.join(constants.LOCAL_FOLDER_BERT, "bert_config.json")
-    with open(bert_config_file, "r+") as f:
-        config_dict = json.loads(f.read())
-
+    config_dict = json.loads(open(bert_config_file, "r+").read())
     bert_config = bert.configs.BertConfig.from_dict(config_dict)
 
-    # bert_classifier, bert_encoder = bert.bert_models.classifier_model(
-    #    bert_config, num_labels=2)
-    print("Bert config loaded.")
-    model = multiword_model(bert_config)
-    print("Model loaded")
+    model = multi_word_model(bert_config)
     tf.keras.utils.plot_model(model, show_shapes=True, dpi=48)
 
 
-def multiword_model(bert_config,
-                    max_seq_length=128,
-                    hub_url_bert_encoder=constants.HUB_URL_BERT,
-                    hub_module_trainable=True,
-                    final_layer_initializer=None):
+def multi_word_model(bert_config,
+                     max_seq_length=128,
+                     hub_url_bert_encoder=constants.HUB_URL_BERT,
+                     hub_module_trainable=True,
+                     final_layer_initializer=None):
     if final_layer_initializer is not None:
         initializer = final_layer_initializer
     else:
@@ -59,10 +49,12 @@ def multiword_model(bert_config,
     pooled_out = tf.keras.layers.Dense(1, kernel_initializer=initializer, name="output_num_words")(
         pooled_out)  # indicate the number of words to generate
 
-    seq_out = tf.keras.layers.Flatten()(seq_out) # TODO find out how I can dynamically slice this to the row that relates to the [MASK] token
+    seq_out = tf.keras.layers.Flatten()(
+        seq_out)  # TODO find out how I can dynamically slice this to the row that relates to the [MASK] token
     seq_out = tf.keras.layers.Dropout(bert_config.hidden_dropout_prob)(
         seq_out)  # TODO should this be the same dropout prob?
-    seq_out = tf.keras.layers.Dense(bert_config.hidden_size,kernel_initializer=initializer)(seq_out) # TODO smaller intermediate layer for less weights, how much can and should I increase this?
+    seq_out = tf.keras.layers.Dense(bert_config.hidden_size, kernel_initializer=initializer)(
+        seq_out)  # TODO smaller intermediate layer for less weights, how much can and should I increase this?
     seq_out = tf.keras.layers.Dense(bert_config.vocab_size, kernel_initializer=initializer, name="output_order_words")(
         seq_out)
 
@@ -79,4 +71,4 @@ def multiword_model(bert_config,
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(plot_model)
